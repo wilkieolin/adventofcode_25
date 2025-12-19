86f182e55eb88faefed77d49e97751ddaf2a154f
Wilkie Olin-Ammentorp <wolinamm@gmail.com>
2025-12-18 23:06:13 -0600

    making it nicer

--- day2.jl
+++ day2.jl
@@ -9,26 +9,6 @@ function extract_range(line::AbstractString)
     return start:stop
 end
 
-function is_valid(id::Int)
-    s = string(id)
-    n_digits = length(s)
-    if mod(n_digits, 2) != 0
-        return 0
-    end
-
-    halfway = floor(Int, n_digits / 2)
-    front = s[1:halfway]
-    back = s[halfway+1:end]
-    if front == back
-        return id
-    end
-    return 0
-end
-
-function invalid_ids_sum(range::UnitRange)
-    return sum(is_valid.(range))
-end
-
 function easy_factor(i::Int)
     possibilities = collect(1:i)
     remainders = mod.(i, possibilities)
@@ -37,10 +17,19 @@ function easy_factor(i::Int)
     return useful_factors
 end
 
-function is_valid_2(id::Int)
+function is_valid(id::Int, even_only::Bool = false)
     s = string(id)
     n_digits = length(s)
-    factors = easy_factor(n_digits)
+    if even_only
+        if mod(n_digits, 2) == 0
+            factors = Set((floor(Int, n_digits/2,)))
+        else
+            return 0
+        end
+    else
+        factors = easy_factor(n_digits)
+    end
+
     for f in factors
         substrings = collect(partition(s, f))
         all_same = foldl((x, y) -> x == y ? x : "", substrings)
@@ -51,11 +40,15 @@ function is_valid_2(id::Int)
     return 0
 end
 
-function invalid_ids_sum_2(range::UnitRange)
-    return sum(is_valid_2.(range))
+function invalid_ids_sum_even(range::UnitRange)
+    return sum(is_valid.(range, true))
+end
+
+function invalid_ids_sum(range::UnitRange)
+    return sum(is_valid.(range, false))
 end
 
 text = readdlm("day2_input.txt", ',')
 ranges = extract_range.(text)
-println("First sum: ", sum(invalid_ids_sum.(ranges)))
-println("Second sum: ", sum(invalid_ids_sum_2.(ranges)))
+println("First sum: ", sum(invalid_ids_sum_even.(ranges)))
+println("Second sum: ", sum(invalid_ids_sum.(ranges)))
